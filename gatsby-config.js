5497478ae23ee028f268aba7feeec5e1c897b168
Shaughn Finnerty <shaughn.finnerty@gmail.com>
2019-09-04 22:01:06 -0400

    Fix site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Shaughn Finnerty - Full Stack Software Developer",
+    author: "Shaughn Finnerty",
+    description: "A portfolio website for myself."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
